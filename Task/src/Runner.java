249033074d3b03f19a5110edd72e9ccb206559fd
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-04 17:20:40 +0300

    Updated

--- Task/src/Runner.java
+++ Task/src/Runner.java
@@ -1,4 +1,10 @@
 public class Runner {
+    static final String JDBC_DRIVER = "sun.jdbc.odbc.JdbcOdbcDriver";
+    static final String DB_URL = "jdbc:odbc:people";
+
+    static final String USER = "";
+    static final String PASS = "";
+
     public static void main(String[] args) {
 
     }
