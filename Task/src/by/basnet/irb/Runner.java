63ab22f94de7375b3b3cfe4aec43625b2bea69d4
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-06 23:39:43 +0300

    Updated

--- Task/src/by/basnet/irb/Runner.java
+++ Task/src/by/basnet/irb/Runner.java
@@ -3,26 +3,25 @@ package by.basnet.irb;
 import java.sql.*;
 
 public class Runner {
-    static final String JDBC_DRIVER = "sun.jdbc.odbc.JdbcOdbcDriver";
-    static final String DB_URL = "jdbc:odbc:coordinates";
-
-    static final String USER = "";
-    static final String PASS = "";
+    static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/mysql";
+    static final String USER = "root";
+    static final String PASS = "root";
 
     public static void main(String[] args) {
         Connection conn = null;
         Statement stmt = null;
-
         try{
-            Class.forName(JDBC_DRIVER);
-            System.out.println("Connecting to database...");
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
-            String sql = "SELECT id, x1, x2 FROM coordinates";
-            ResultSet rs = stmt.executeQuery(sql);
+            String sql = "SELECT ROUND(ABS((x1 - x2)+0.5)) AS len, Count(*) AS num FROM coord GROUP BY ROUND(ABS((x1 - x2)+0.5)) ORDER BY ROUND(ABS((x1 - x2)+0.5)) ASC";
 
-        } catch (ClassNotFoundException | SQLException e) {
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()){
+
+            }
+
+        } catch (SQLException e) {
             System.out.println("Connection error: " + e.toString());
         }
 
