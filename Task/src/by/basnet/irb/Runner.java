0aef5f91bf7f9f0844981ecc29d604f48a2de147
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-07 14:56:03 +0300

    First part is done

--- Task/src/by/basnet/irb/Runner.java
+++ Task/src/by/basnet/irb/Runner.java
@@ -13,25 +13,38 @@ public class Runner {
         Connection conn = null;
         Statement stmt = null;
         List<Segment> list = new ArrayList<>();
-        //1
-        try{
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
             stmt = conn.createStatement();
-            String sql = "SELECT ROUND(ABS((x1 - x2)+0.5)) AS len, Count(*) AS num FROM coordinates.coord GROUP BY ROUND(ABS((x1 - x2)+0.5)) ORDER BY ROUND(ABS((x1 - x2)+0.5)) ASC";
-            ResultSet rs = stmt.executeQuery(sql);
-            while (rs.next()){
-                int len = (int) rs.getDouble("len");
-                int num = rs.getInt("num");
-                Segment segment = new Segment(len, num);
-                list.add(segment);
+            //1
+            try {
+                String sql = "SELECT ROUND(ABS((x1 - x2)+0.5)) AS len, Count(*) AS num FROM coordinates.coord GROUP BY ROUND(ABS((x1 - x2)+0.5)) ORDER BY ROUND(ABS((x1 - x2)+0.5)) ASC";
+                ResultSet rs = stmt.executeQuery(sql);
+                while (rs.next()) {
+                    int len = (int) rs.getDouble("len");
+                    int num = rs.getInt("num");
+                    Segment segment = new Segment(len, num);
+                    list.add(segment);
+                }
+                for (Segment segment : list)
+                    System.out.println(segment);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Wrong column index:" + e.toString());
+            } catch (SQLException e) {
+                System.out.println("Wrong SQL query : " + e.toString());
+            } finally {
+                try {
+                    if (stmt != null)
+                        stmt.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
             }
-            for(Segment segment: list)
-                System.out.println(segment);
-        } catch (SQLException e) {
-            System.out.println("Wrong SQL query : " + e.toString());
-        } catch (IndexOutOfBoundsException e){
-            System.out.println("Wrong column index:" + e.toString());
+            //2
+            stmt = conn.createStatement();
+            stmt.executeUpdate("TRUNCATE TABLE coordinates.frequencies");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-
     }
 }
