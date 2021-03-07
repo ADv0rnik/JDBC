16aac810435233762945c88b0bd05d0b95f74e46
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-07 11:06:05 +0300

    Found proper SQL query

--- Task/src/by/basnet/irb/Runner.java
+++ Task/src/by/basnet/irb/Runner.java
@@ -1,6 +1,8 @@
 package by.basnet.irb;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Runner {
     static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/mysql";
@@ -10,19 +12,25 @@ public class Runner {
     public static void main(String[] args) {
         Connection conn = null;
         Statement stmt = null;
+        List<Segment> list = new ArrayList<>();
+        //1
         try{
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            System.out.println("Creating statement...");
             stmt = conn.createStatement();
-            String sql = "SELECT ROUND(ABS((x1 - x2)+0.5)) AS len, Count(*) AS num FROM coord GROUP BY ROUND(ABS((x1 - x2)+0.5)) ORDER BY ROUND(ABS((x1 - x2)+0.5)) ASC";
-
+            String sql = "SELECT ROUND(ABS((x1 - x2)+0.5)) AS len, Count(*) AS num FROM coordinates.coord GROUP BY ROUND(ABS((x1 - x2)+0.5)) ORDER BY ROUND(ABS((x1 - x2)+0.5)) ASC";
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()){
-
+                int len = (int) rs.getDouble("len");
+                int num = rs.getInt("num");
+                Segment segment = new Segment(len, num);
+                list.add(segment);
             }
-
+            for(Segment segment: list)
+                System.out.println(segment);
         } catch (SQLException e) {
-            System.out.println("Connection error: " + e.toString());
+            System.out.println("Wrong SQL query : " + e.toString());
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("Wrong column index:" + e.toString());
         }
 
     }
